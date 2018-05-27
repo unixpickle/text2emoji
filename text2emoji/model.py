0496e8ba815fd2d4d4dbe2ef7f8c3e8c450dfdea
Alex Nichol <unixpickle@gmail.com>
2018-05-27 19:13:50 -0400

    bigger model
    Current reconstructions look a bit blotchy

--- text2emoji/model.py
+++ text2emoji/model.py
@@ -24,9 +24,12 @@ def generate_images(embeddings, platforms, training=False):
         out = tf.nn.relu(out)
         return out
 
-    out = tf.layers.conv2d(out, 32, 3, activation=activation, padding='same')
+    out = tf.layers.conv2d(out, 64, 3, activation=activation, padding='same')
     for depth in [128, 64, 32]:
-        out = tf.layers.conv2d(out, depth, 3, activation=activation, padding='same')
+        out_1 = tf.layers.conv2d(out, depth, 3, activation=activation, padding='same')
+        out_2 = tf.layers.conv2d(out_1, depth, 3, padding='same')
+        out_2 = tf.layers.batch_normalization(out_2, training=training)
+        out = tf.nn.relu(out + out_2)
         out = tf.layers.conv2d_transpose(out, depth, 3, strides=2, activation=activation,
                                          padding='same')
     out = tf.layers.conv2d(out, 4, 3, padding='same')
